b382e7d4ef93f5473925d32e8ae1f21da2ddd981
Stefano Lottini <stefano.lottini@datastax.com>
2021-10-11 19:12:03 +0200

    spiders

--- client/src/components/GameField.js
+++ client/src/components/GameField.js
@@ -1,14 +1,27 @@
 const GameField = ({playerMap, playerID, boardWidth, boardHeight}) => {
 
+  // const plColor = thatPlayerID === playerID ? '#b0b0f0' : '#b0f0b0'
+
   return (
-    <svg width="400" height="300" viewBox={`0 0 ${100 * boardWidth} ${100 * boardHeight}`}>\
-      <rect width={100 * boardWidth} height={100 * boardHeight} style={{fill: '#f3ffab'}} />
+    <svg width="600" height="450" viewBox={`0 0 ${100 * boardWidth} ${100 * boardHeight}`}>
+      <defs>
+       <pattern id="lyco_other" x="50" y="50" width="100" height="100" patternUnits="userSpaceOnUse">
+         <image x="0" y="0" width="100" height="100" href="lyco_other.svg"></image>
+       </pattern>
+       <pattern id="lyco_self" x="50" y="50" width="100" height="100" patternUnits="userSpaceOnUse">
+         <image x="0" y="0" width="100" height="100" href="lyco_self.svg"></image>
+       </pattern>
+      </defs>
+      <rect width={100 * boardWidth} height={100 * boardHeight} style={{fill: '#f9ffbb'}} />
       { Object.entries(playerMap).map( ([thatPlayerID, thatPlayerInfo]) => {
-        const plColor = thatPlayerID === playerID ? '#b0b0f0' : '#b0f0b0'
+        const patternName = thatPlayerID === playerID ? 'lyco_self' : 'lyco_other'
         return (<g key={thatPlayerID} transform={`translate(${thatPlayerInfo.x * 100},${thatPlayerInfo.y * 100})`}>
-          <circle cx="50" cy="50" r="40" fill={plColor} />
+          {/*<circle cx="50" cy="50" r="40" fill={plColor} />*/}
           <g transform='translate(50,50)'>
-            <text textAnchor='middle' fill='#404040' fontSize='40'>{thatPlayerInfo.playerName}</text>
+            <circle cx="0" cy="0" r="50" fill={`url(#${patternName})`}></circle>
+            <g transform='translate(0,50)'>
+              <text textAnchor='middle' fill='#404040' fontSize='40'>{thatPlayerInfo.playerName}</text>
+            </g>
           </g>
         </g>)
       })}
