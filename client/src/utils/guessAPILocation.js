b9147e30a75f1eb5d8d72bc7353123c333e2d575
Stefano Lottini <stefano.lottini@datastax.com>
2021-11-02 14:56:34 +0100

    gitpod URL guesser workaround

--- client/src/utils/guessAPILocation.js
+++ client/src/utils/guessAPILocation.js
@@ -2,7 +2,14 @@
 // (in a rather crude manner)
 const guessAPILocation = (clientURL, defaultURL) => {
   if ((clientURL.indexOf('3000') >= 0) && (clientURL.indexOf('http') >= 0)){
-    const apiLoc = clientURL.replace('3000', '8000').replace('http', 'ws')
+
+    // this cleanup is to make the guess work within Gitpod, where the "(mini-)browser" reports an URL
+    // such as "wss://8000-apricot-lizard-sa8u19n5.ws-eu17.gitpod.io/?vscodeBrowserReqId=1635860969945"
+    // (as opposed to "ordinary" browsers where the URL would end with "...gitpod.io/")
+    const gpCleanedClientURL = clientURL.indexOf('?') >= 0 ? clientURL.slice(0, clientURL.indexOf('?')) : clientURL
+
+    // now the 'standard' guesswork can start
+    const apiLoc = gpCleanedClientURL.replace('3000', '8000').replace('http', 'ws')
     if (apiLoc[apiLoc.length - 1] === '/'){
       // get rid of trailing '/'
       return apiLoc.slice(0, -1)
